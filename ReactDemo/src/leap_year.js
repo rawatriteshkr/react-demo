dfe08ef99128df7d3bb9a62a22600ba957209cd7
admin <admin@DESKTOP-82POU4M>
2021-01-20 15:28:58 +0530

    leap year

---
+++ ReactDemo/src/leap_year.js
@@ -0,0 +1,28 @@
+//Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+import React from 'react';
+
+class LeapYear extends React.Component{
+
+	leapYear = () =>{
+		var year = document.getElementById('year').value;
+		if((year%4 == 0) || ((year%100 == 0)) && (year%400 == 0)){
+			document.getElementById('print_message').innerHTML = 'Leap Year';
+		}else{
+			document.getElementById('print_message').innerHTML = 'Not Leap Year';
+		}
+	}
+
+	render(){
+		return(
+			<div>
+				<h3>Find year is a leap year or not.</h3>
+				<label for='year'>Year : </label>
+				<input id='year' type='text' maxLength='4'></input>
+				<button type='button' onClick={this.leapYear}>Click</button>
+				<p id='print_message'></p>
+			</div>
+		);
+	}
+}
+
+export default LeapYear;
